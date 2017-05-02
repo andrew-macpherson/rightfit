a98bd9dd973336286936a426b920e6a63014cef6
Andrew MacPherson <andrewmac17@gmail.com>
2017-05-01 23:56:55 -0400

    registration handler

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -6,18 +6,21 @@ var $$ = Dom7;
 
 // Add view
 var mainView = app.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
 
 
-$$('form.regitration-form').on('form:success', function (e) {
-    var xhr = e.detail.xhr; // actual XHR object
-    console.log(xrh);
 
-    var data = e.detail.data; // Ajax response from action file
-    // do something with response data
-    console.log(data);
-    alert('registration success');
-    return false;
+app.onPageInit( 'register', function( page ) {
+    $$('#registrationFormSubmit').on( 'click', registerHandler );
+}).trigger();
+
+app.onPageBeforeRemove( 'register', function( page ) {
+    $$('#registrationFormSubmit').off( 'click', registerHandler );
 });
+
+
+function registerHandler(){
+	var formData = app.formToData('#regitrationForm');
+	alert(JSON.stringify(formData));
+}
