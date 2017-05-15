a9d4593adb8f64c2f23a45b548f9f4a71193534c
Andrew MacPherson <andrewmac17@gmail.com>
2017-05-14 23:01:03 -0400

    ui updates / react start

--- app/src/pages/dashboard.js
+++ app/src/pages/dashboard.js
@@ -20,6 +20,8 @@ rightFit.Pages.Dashboard = (function(){
 			dateCalendar = app.calendar({
 			    input: '#currentDay'
 			});
+
+			getActivities();
 		});
 
 		app.onPageBeforeRemove( 'dashboard', function( page ) {
@@ -27,6 +29,63 @@ rightFit.Pages.Dashboard = (function(){
 		    $$('.dateYesterday').off( 'click', setDateYesterday );
 		    $$('.dateTomorrow').off( 'click', setDateTomorrow );
 		});
+
+	}
+
+	function getActivities(){
+		/*
+		var ListItems = React.createClass({displayName: 'ListItems',
+			render: function() {
+				return (
+					React.DOM.div({className: "page-content"}, 
+						React.DOM.div({className: "list-block contacts-list"}, 
+							React.DOM.ul(null, items)
+						)
+					)
+				);
+			}
+		});
+
+		React.renderComponent(ListItems(), document.getElementById('cardioActivities'));
+		*/
+
+
+
+		/*
+		var activities = [
+		  {id: 1, name: "sprint", time: "30 min"},
+		  {id: 2, name: "jog", time: "90 min"},
+		  {id: 3, name: "spin bike", time: "30 min"},
+		]
+
+		var activityItems = React.createClass({
+		  propTypes: {
+		    name: React.PropTypes.string.isRequired,
+		    time: React.PropTypes.string
+		  },
+
+		  render: function() {
+		    // I wrap mult-line return statements in parentheses to avoid the
+		    // inevitable bugs caused by forgetting that JavaScript will throw away
+		    // the final lines when possible. The parentheses are not strictly
+		    // necessary.
+		    return (
+		      React.createElement('tr', {}, 
+			    React.createElement('td', {className:'label-cell'}, this.name),
+			    React.createElement('td', {className:'numeric-cell'}, this.time)
+			  )
+		    )
+		  },
+		})
+
+		var activityItemElements = activities
+		.filter(function(activity) { return activity.name })
+		.map(function(activity) { return React.createElement(activityItems, activity) })
+
+
+		var rootElement = React.createElement('tbody', {}, activityItemElements);
+		ReactDOM.render(rootElement, document.getElementById('cardioActivities'));
+		*/
 
 	}
 
